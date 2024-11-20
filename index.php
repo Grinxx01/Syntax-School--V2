87dd87f3cc46fc28e3c9c544515986edd024f90f
Grizzzly <aatrox543@gmail.com>
2024-11-20 13:11:47 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SyntaxSchool</title>
+</head>
+<body>
+    <header>
+        <h1>Syntax School</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Tutorial</a></li>
+                <li><a href="#">Latihan</a></li>
+                <li><a href="#">Chalenge</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
